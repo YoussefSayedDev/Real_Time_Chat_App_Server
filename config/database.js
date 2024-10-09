d3ceb0c50bc9794304c7a06a2ba2b018c0d02c24
YoussefSayedDev <youssef.el.sayed.email@gmail.com>
2024-10-09 13:52:06 +0300

    some chages

--- config/database.js
+++ config/database.js
@@ -2,12 +2,16 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 // Create a new pool instance with evironment variable
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT,
+// });
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.POSTGRES_URL,
 });
 
 pool
